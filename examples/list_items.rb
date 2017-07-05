d2ccd41b85c047cf2fe70bc9669ca70c8678277a
Peter Graham <peterghm@gmail.com>
2017-07-05 17:54:37 -0400

    better examples

---
+++ examples/list_items.rb
@@ -0,0 +1,18 @@
+require "dotenv/load"
+require "pp"
+require "bundler/setup"
+require "rollbar-api"
+
+project_name = ENV["ROLLBAR_PROJECT_NAME"] or raise "Must specify ROLLBAR_PROJECT_NAME in .env"
+project_access_token = ENV["ROLLBAR_PROJECT_ACCESS_TOKEN"] or raise "Must specify ROLLBAR_PROJECT_ACCESS_TOKEN in .env"
+
+RollbarApi::Project.add(project_name, project_access_token)
+
+# Fetch all items
+items = RollbarApi::Project.find(project_name).get("/api/1/items")
+
+# Print out the first 5 items
+items.result.items.first(5).each do |item|
+  pp item
+  puts "=" * 80
+end
---
+++ examples/rql_query.rb
@@ -0,0 +1,23 @@
+require "dotenv/load"
+require "pp"
+require "bundler/setup"
+require "rollbar-api"
+
+project_name = ENV["ROLLBAR_PROJECT_NAME"] or raise "Must specify ROLLBAR_PROJECT_NAME in .env"
+project_access_token = ENV["ROLLBAR_PROJECT_ACCESS_TOKEN"] or raise "Must specify ROLLBAR_PROJECT_ACCESS_TOKEN in .env"
+
+RollbarApi::Project.add(project_name, project_access_token)
+
+# Create the job
+rql_job = RollbarApi::Project.find(project_name).post("/api/1/rql/jobs", {
+  query_string: "select * from item_occurrence where item.counter=1",
+})
+
+# Fetch job status
+rql_job = RollbarApi::Project.find(project_name).get("/api/1/rql/job/#{rql_job.result.id}")
+pp rql_job
+
+if rql_job.result.status == "success"
+  # Print out succeeded job result
+  pp RollbarApi::Project.find(project_name).get("/api/1/rql/job/#{rql_job.result.id}/result")
+end
